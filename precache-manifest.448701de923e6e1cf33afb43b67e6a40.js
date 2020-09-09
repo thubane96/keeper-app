eaa763ecafc247a14af10fd9b8b62c5b166c400b
Musa <58530808+thubane96@users.noreply.github.com>
2020-09-09 08:13:17 +0200

    Updates

---
+++ precache-manifest.448701de923e6e1cf33afb43b67e6a40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5108a4f272ba07bc4ed3fecbc6e0e3c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "647cc7563b30a9df6673",
+    "url": "/keeper-app/static/js/2.99d717be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keeper-app/static/js/2.99d717be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a980380e09887e0f0f8",
+    "url": "/keeper-app/static/js/main.717849b1.chunk.js"
+  },
+  {
+    "revision": "44d6fa134b32989acb5c",
+    "url": "/keeper-app/static/js/runtime-main.3313fec8.js"
+  }
+]);
